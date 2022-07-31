24232b3feaa419d87e9b066f7e783e2f0e018755
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-07-31 14:48:29 +0530

    Added the Category item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './categories.styles.scss'
+import CategoryItem from './components/category-item/category-item.component'
 
 const App = () => {
   const categories =[
@@ -31,15 +32,11 @@ const App = () => {
   ]
     return (
     <div className='categories-container'>
-      {categories.map(({title,id,imageUrl}) => (
-      <div key={id} className='category-container'>
-     <div className='background-image' style= {{backgroundImage : `url(${imageUrl})`}}/>
-       <div className='category-body-container'>
-         <h2>{title}</h2>
-         <p>Shop Now </p>
-       </div>
-      </div>
-   ))}
+      {
+        categories.map(category => (
+          <CategoryItem key={category.id} category={category}/>
+        ))
+      }
     </div>
   )
 }
