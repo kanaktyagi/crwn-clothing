183280a4bfefa16caae2ef01c47a46da9a1f9e83
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-08-15 14:04:41 +0530

    Added the cart-item logic to be deplayed in the drop down and fixed the quantity number in the cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../../assests/shopping-bag.svg";
+import { ToggleCartContext } from "../../contexts/toggle-cart.context";
 
 import './cart-icon.styles.scss';
 
 
 const CartIcon = () => {
-    return(
+    const { cartItems } = useContext(ToggleCartContext)
+    const quantity = cartItems.reduce((acc, item) => { return acc + item.quantity }, 0)
+    return (
         <div className="cart-icon-container">
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{quantity}</span>
         </div>
     )
 }
