8f9eda65481839c1b75577e4e3ac2909017b639b
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-08-16 19:54:50 +0530

    Moved the checkout Items into a separate component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { ToggleCartContext } from '../../contexts/toggle-cart.context'
+import './checkout.styles.scss'
 
 const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } = useContext(ToggleCartContext)
@@ -7,18 +9,29 @@ const Checkout = () => {
     const removeItem = (id) => removeItemFromCart(id)
 
     return (
-        <div>
-            <div>
-                {cartItems.map(cartItem => {
-                    const { id, name, quantity } = cartItem
-                    return (
-                        <div key={id}>
-                            <h2>{name}</h2>
-                            <span onClick={() => removeItem(id)}> - </span> <span>{quantity}</span> <span onClick={() => addItem(cartItem)}> +</span>
-                        </div>
-                    )
-                })}
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
+
+            {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+
+            )}
+            <span className='Total'>Total: 0</span>
         </div>
     )
 }
