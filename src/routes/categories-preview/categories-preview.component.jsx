1702348d4e8874fd1b23b5845523d6470e816d88
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-08-21 15:10:34 +0530

    Added the individual route in the Shop component

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react"
+import CategoryPreview from "../../category-preview/category-preview.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+
+
+const CategoriesPreview = () => {
+    const { categoriesMap } = useContext(CategoriesContext)
+    return (
+        <div>
+            {
+                Object.keys(categoriesMap).map(title => {
+                    const products = categoriesMap[title];
+
+                    return <CategoryPreview key={title} title={title} products={products} />
+                })
+            }
+
+        </div>
+    )
+}
+
+export default CategoriesPreview 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,14 @@
-import { useContext } from "react"
-import CategoryPreview from "../../category-preview/category-preview.component"
-import ProductCard from "../../components/product-card/product-card.component"
-import { CategoriesContext } from "../../contexts/categories.context"
+import { Routes, Route } from 'react-router-dom'
+import CategoriesPreview from '../categories-preview/categories-preview.component'
+import Category from '../category/category.component'
 import './shop.styles.scss'
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext)
     return (
-        <div className="shop-contanier">
-            {
-                Object.keys(categoriesMap).map(title => {
-                    const products = categoriesMap[title];
-
-                    return <CategoryPreview key={title} title={title} products={products} />
-                })
-            }
-
-        </div>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
     )
 }
 
