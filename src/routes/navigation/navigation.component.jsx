fc539c8dcd77cb62c2d98e4f04ed151d08260f85
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-08-22 22:14:57 +0530

    Used the useSelector hooks

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,46 +1,52 @@
-import {Outlet,Link} from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import './navigation.styles.scss'
 import { ReactComponent as Crwnlogo } from '../../assests/crown.svg'
 import { useContext } from 'react'
-import { UserContent } from '../../contexts/user.context'
+// import { UserContent } from '../../contexts/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ToggleCartContext } from '../../contexts/toggle-cart.context'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '../../store/user/user.selector'
 
-const Navigation =() => {
-    const {currentUser} = useContext(UserContent)
-    const {toggleCart, changeToggleCart} = useContext(ToggleCartContext)
+const Navigation = () => {
+   // const { currentUser } = useContext(UserContent)
+   const { toggleCart, changeToggleCart } = useContext(ToggleCartContext)
+   const currentUser = useSelector(selectCurrentUser)
 
-    
-    return (
+
+
+
+
+   return (
       <>
-        <div className='navigation'>
+         <div className='navigation'>
             <Link className='logo-container' to='/'>
-            <Crwnlogo className='logo'/>
+               <Crwnlogo className='logo' />
             </Link>
             <div className='nav-links-container'>
-             <Link className='nav-link' to="/shop">
-                SHOP
-             </Link>
-             {currentUser ? (
-                <span className='nav-link' onClick={signOutUser}>SIGN-OUT</span>
-             ): (
-                <Link className='nav-link' to="/auth">
-                SIGN-IN
-             </Link>
-             )}
-              <div onClick={changeToggleCart}>
-            <CartIcon/>
+               <Link className='nav-link' to="/shop">
+                  SHOP
+               </Link>
+               {currentUser ? (
+                  <span className='nav-link' onClick={signOutUser}>SIGN-OUT</span>
+               ) : (
+                  <Link className='nav-link' to="/auth">
+                     SIGN-IN
+                  </Link>
+               )}
+               <div onClick={changeToggleCart}>
+                  <CartIcon />
+               </div>
             </div>
-            </div>
-           
-           {toggleCart &&  <CartDropdown/>}
-          
-        </div>
-        <Outlet/>
-      </>
-    )
-  }
 
-  export default Navigation
+            {toggleCart && <CartDropdown />}
+
+         </div>
+         <Outlet />
+      </>
+   )
+}
+
+export default Navigation
